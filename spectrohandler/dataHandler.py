db4e36227073a4b94e48a75a22fdbc0026dd7e59
Filip O <filip.kenay@gmail.com>
2021-02-12 16:37:33 +0100

    return ordered and limited acquisition dataframe

--- spectrohandler/dataHandler.py
+++ spectrohandler/dataHandler.py
@@ -1,41 +1,24 @@
-#Read .txt file
-#Write to db
-#Read from db
-
-import logging
-import requests
-
-import sqlite3
 import numpy as np
 import pandas as pd
 
-# DO NOT USE - USE CSV INSTEAD
-#Access row columns by name
-def dict_factory(cursor, row):
-    d = {}
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
-db = sqlite3.connect('./spectroscopy.sqlite')
-db.row_factory = dict_factory
-
 #Can aggregate by None, Frequencies, Coordinate Range, Coordinate Range AND Frequencies
-def aggregateAcquistion(seriesId, *, frequencies, coordRange):
-    #Set default variables
-    series = requests.get('http://localhost:5500/series/get-series?seriesId='+str(seriesId))
-    radius = series['Radius']
-    frequency = [0, 2000]
+def aggregateAcquistion(title, *, frequencies, coordRange):
+    #Get series dataframe
+    df = getSeriesDataframe(title)
+    radius = df['x'].max()
+    frequency = [0, 9999]
     range = {'rangeX': [-radius, radius],
             'rangeY': [-radius, radius]}
+
     #Set parameter variables
     if frequencies != None:
         frequency = frequencies
     if coordRange != None:    
         try:
-            range.rangeX[0] = min(coordRange.rangeX)
-            range.rangeX[1] = max(coordRange.rangeX)
-            range.rangeY[0] = min(coordRange.rangeY)
-            range.rangeY[1] = max(coordRange.rangeY)
+            range['rangeX'][0] = min(coordRange['rangeX'])
+            range['rangeX'][1] = max(coordRange['rangeX'])
+            range['rangeY'][0] = min(coordRange['rangeY'])
+            range['rangeY'][1] = max(coordRange['rangeY'])
         except:
             range = {'rangeX': [-radius, radius],
                     'rangeY': [-radius, radius]}
@@ -43,29 +26,24 @@ def aggregateAcquistion(seriesId, *, frequencies, coordRange):
                 values {'rangeX':[min,max],'rangeY':[min, max]}, 
                 got """+str(coordRange)+""" instead""")
     
-    #Aggregate by range,frequency
-    selectAggregate = """SELECT * FROM Acquisition WHERE
-        (CoordX > (?) AND CoordX < (?)) AND
-        (CoordY > (?) AND CoordY < (?)) AND
-        (Frequency > (?) AND Frequency < (?)) AND
-        (SeriesId = (?))
-        ORDER BY Frequency ASC, CoordX ASC, CoordY ASC"""
-    aggrTbl = db.execute(selectAggregate, [range.rangeX[0], range.rangeX[1],
-                                 range.rangeY[0], range.rangeY[1],
-                                 frequency[0], frequency[1],
-                                 seriesId]).fetchall()
-    #Create [][] for rows, [str] for column names
-    aggrArr = []
-    aggrCols = ['x','y']
-    for row in aggrTbl:
-        isInObj = False
-        for coord in aggrArr:
-            if coord[0] == row['CoordX'] and coord[1] == row['CoordY']:
-                aggrObj[coord][row['Frequency']] = row['Intensity']
-                isInObj = True
-        if row['Frequency'] not in aggrCols:
-            aggrCols.append(row['Frequency'])
-        if not isInObj:
-            aggrArr.append([row['CoordX'], row['CoordY'], row['Intensity']])
-    #Create DF from rows and column names
-    return pd.DataFrame(np.array(aggrArr, columns=aggrCols))
+    #Mask dataframe to XY
+    df['x'] = pd.to_numeric(df['x'])
+    df['y'] = pd.to_numeric(df['y'])
+    mask = ((df['x'] >= range['rangeX'][0]) & (df['x'] <= range['rangeX'][1]) &
+            (df['y'] >= range['rangeY'][0]) & (df['y'] <= range['rangeY'][1]))
+    df = df.loc[mask]
+
+    #Mask dataframe to Frequency
+    df = pd.melt(df.iloc[:,1:], id_vars=['x', 'y'], var_name='frequency',
+        value_name='intensity').sort_values(['x', 'y', 'frequency'])
+
+    df['frequency'] = pd.to_numeric(df['frequency'])
+    mask = ((df['frequency'] >= frequency[0]) & (df['frequency'] <= frequency[1]))
+    df = pd.pivot_table(df.loc[mask],values='intensity',index=['x','y'],columns='frequency').reset_index()
+    
+    #return dataframe
+    return df
+
+def getSeriesDataframe(title):
+    csv = '../csv/{}.csv'.format(title)
+    return pd.read_csv(csv, index_col=False)
