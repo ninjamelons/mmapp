5e8dac816c79eebe49733da0a7f826b272196189
Filip Olsen <filip.kenya@gmail.com>
2021-03-03 12:49:11 +0100

    pyinstaller works on mmapp.py, monkeypatch+datas

--- mmapp.py
+++ mmapp.py
@@ -2,11 +2,38 @@
 import time
 from threading import Thread
 
+#Monkey patch flask-compress
+#*********************************************#
+import sys
+from collections import namedtuple
+
+import pkg_resources
+
+IS_FROZEN = hasattr(sys, '_MEIPASS')
+
+# backup true function
+_true_get_distribution = pkg_resources.get_distribution
+# create small placeholder for the dash call
+# _flask_compress_version = parse_version(get_distribution("flask-compress").version)
+_Dist = namedtuple('_Dist', ['version'])
+
+def _get_distribution(dist):
+    if IS_FROZEN and dist == 'flask-compress':
+        return _Dist('1.5.0')
+    else:
+        return _true_get_distribution(dist)
+
+# monkey patch the function so it can work once frozen and pkg_resources is of
+# no help
+pkg_resources.get_distribution = _get_distribution
+#**********************************************#
+
 from transmitter.transmitter_service import init_rest_service
 from transmitter.spectrohandler.gui_init import init_gui, start_webview
 
 def run_service():
     init_rest_service()
+    
 
 def run_gui():
     app = init_gui()
