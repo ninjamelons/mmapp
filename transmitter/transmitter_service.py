4b607e6c14ccc34559818090f431b1e33468abba
Filip O <filip.kenay@gmail.com>
2021-04-07 12:16:32 +0200

    version+

--- transmitter/transmitter_service.py
+++ transmitter/transmitter_service.py
@@ -53,7 +53,7 @@ app = FastAPI(
     title="Transmitter Service",
     description="""Receives requests from LabSpec to acquire spectroscopy data
          and move the microscope stage through micromanager""",
-    version="1.2.0",
+    version="1.2.5",
     openapi_tags=api_tags
 )
 
