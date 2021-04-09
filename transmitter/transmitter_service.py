140b370dd10c6be45b357905d8bd867e1323b906
Filip O <filip.kenay@gmail.com>
2021-04-09 13:51:48 +0200

    adjust movement params

--- transmitter/transmitter_service.py
+++ transmitter/transmitter_service.py
@@ -194,7 +194,7 @@ async def moveStageSequence(seriesId: int):
 
         #Move stage next position
         epsilon = 0.1*interval # Accepted difference between expected and actual pos
-        maxTries = 4 # Max no. attempts to move stage per epsilon
+        maxTries = 5 # Max no. attempts to move stage per epsilon
         nTries = 0 # No. attempts moved this epsilon
         xMvDiff = 0 # Failed x movement difference (%, 0-1)
         yMvDiff = 0 # Failed x movement difference (%, 0-1)
@@ -239,7 +239,7 @@ async def moveStageSequence(seriesId: int):
             
             # Check no. tries exceeds max tries
             if nTries >= maxTries:
-                epsilon += (0.1*interval)
+                epsilon += (0.2*interval)
                 #Reset nTries and diff error rate
                 nTries = 0
                 xMvDiff = 0
